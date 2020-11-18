e177d738306417ae613d06723dd9942c2cc6a164
florian260000 <florianbartling@live.de>
2020-11-18 17:36:39 +0100

    added webOTA

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,29 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include "login.hpp";
+#include "login.hpp"
+#include <WebOTA.h>
+//https://github.com/scottchiefbaker/ESP-WebOTA
 
 const int oneWireBus = 4;
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  sensors.requestTemperatures();
+  float temperature = sensors.getTempCByIndex(0);
+  String s = "" + (String) temperature + " C";
+  server.send(200, "text/plain", s);
+}
 
 void setup()
 {
   Serial.begin(9600);
+  WiFi.hostname("keller-test-esp");
   WiFi.begin(ssid, password);
   int counter = 0;
-  while (WiFi.status() != WL_CONNECTED && counter < 10)
+  while (WiFi.status() != WL_CONNECTED && counter < 15)
   {
     delay(500);
     counter++;
@@ -23,16 +33,14 @@ void setup()
   if(WiFi.status() != WL_CONNECTED){
     ESP.restart();
   }
+  Serial.println(WiFi.localIP());
   sensors.begin();
+  server.on("/",handleRoot);
   server.begin();
 }
 
 void loop()
 {
-  server.
-  sensors.requestTemperatures();
-  float temperature = sensors.getTempCByIndex(0);
-  Serial.print(temperature);
-  Serial.println("ºC");
-  delay(5000);
+  webota.handle();
+  server.handleClient();
 }
